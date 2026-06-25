35ef238cb1856f8549e530631b87ecef18411ade
Bully <vburthur@googlemail.com>
2026-06-25 15:04:09 +0200

    Added more textx

--- scripts/pf2e-narrative-forge.js
+++ scripts/pf2e-narrative-forge.js
@@ -1,5 +1,5 @@
 const MODULE_ID = "pf2e-narrative-forge";
-const MODULE_VERSION = "0.1.0";
+const MODULE_VERSION = "0.1.1";
 
 const DAMAGE_TYPE_LABELS = {
   acid: "PF2E_NARRATIVE_FORGE.DamageTypes.acid",
@@ -21,37 +21,97 @@ const DAMAGE_NARRATIONS = {
   slashing: [
     "PF2E_NARRATIVE_FORGE.Narration.Damage.slashing.1",
     "PF2E_NARRATIVE_FORGE.Narration.Damage.slashing.2",
-    "PF2E_NARRATIVE_FORGE.Narration.Damage.slashing.3"
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.slashing.3",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.slashing.4",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.slashing.5",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.slashing.6",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.slashing.7",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.slashing.8",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.slashing.9",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.slashing.10",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.slashing.11",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.slashing.12",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.slashing.13",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.slashing.14",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.slashing.15"
   ],
   piercing: [
     "PF2E_NARRATIVE_FORGE.Narration.Damage.piercing.1",
     "PF2E_NARRATIVE_FORGE.Narration.Damage.piercing.2",
-    "PF2E_NARRATIVE_FORGE.Narration.Damage.piercing.3"
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.piercing.3",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.piercing.4",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.piercing.5",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.piercing.6",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.piercing.7",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.piercing.8",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.piercing.9",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.piercing.10",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.piercing.11",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.piercing.12",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.piercing.13",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.piercing.14",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.piercing.15"
   ],
   bludgeoning: [
     "PF2E_NARRATIVE_FORGE.Narration.Damage.bludgeoning.1",
     "PF2E_NARRATIVE_FORGE.Narration.Damage.bludgeoning.2",
-    "PF2E_NARRATIVE_FORGE.Narration.Damage.bludgeoning.3"
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.bludgeoning.3",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.bludgeoning.4",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.bludgeoning.5",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.bludgeoning.6",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.bludgeoning.7",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.bludgeoning.8",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.bludgeoning.9",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.bludgeoning.10",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.bludgeoning.11",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.bludgeoning.12",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.bludgeoning.13",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.bludgeoning.14",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.bludgeoning.15"
   ],
   fire: [
     "PF2E_NARRATIVE_FORGE.Narration.Damage.fire.1",
     "PF2E_NARRATIVE_FORGE.Narration.Damage.fire.2",
-    "PF2E_NARRATIVE_FORGE.Narration.Damage.fire.3"
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.fire.3",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.fire.4",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.fire.5",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.fire.6",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.fire.7",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.fire.8",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.fire.9",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.fire.10",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.fire.11",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.fire.12"
   ],
   cold: [
     "PF2E_NARRATIVE_FORGE.Narration.Damage.cold.1",
     "PF2E_NARRATIVE_FORGE.Narration.Damage.cold.2",
-    "PF2E_NARRATIVE_FORGE.Narration.Damage.cold.3"
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.cold.3",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.cold.4",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.cold.5",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.cold.6",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.cold.7",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.cold.8"
   ],
   electricity: [
     "PF2E_NARRATIVE_FORGE.Narration.Damage.electricity.1",
     "PF2E_NARRATIVE_FORGE.Narration.Damage.electricity.2",
-    "PF2E_NARRATIVE_FORGE.Narration.Damage.electricity.3"
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.electricity.3",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.electricity.4",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.electricity.5",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.electricity.6",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.electricity.7",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.electricity.8"
   ],
   acid: [
     "PF2E_NARRATIVE_FORGE.Narration.Damage.acid.1",
     "PF2E_NARRATIVE_FORGE.Narration.Damage.acid.2",
-    "PF2E_NARRATIVE_FORGE.Narration.Damage.acid.3"
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.acid.3",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.acid.4",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.acid.5",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.acid.6",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.acid.7",
+    "PF2E_NARRATIVE_FORGE.Narration.Damage.acid.8"
   ],
   force: [
     "PF2E_NARRATIVE_FORGE.Narration.Damage.force.1",
@@ -87,7 +147,19 @@ const DAMAGE_NARRATIONS = {
 const CRITICAL_NARRATIONS = [
   "PF2E_NARRATIVE_FORGE.Narration.Critical.1",
   "PF2E_NARRATIVE_FORGE.Narration.Critical.2",
-  "PF2E_NARRATIVE_FORGE.Narration.Critical.3"
+  "PF2E_NARRATIVE_FORGE.Narration.Critical.3",
+  "PF2E_NARRATIVE_FORGE.Narration.Critical.4",
+  "PF2E_NARRATIVE_FORGE.Narration.Critical.5",
+  "PF2E_NARRATIVE_FORGE.Narration.Critical.6",
+  "PF2E_NARRATIVE_FORGE.Narration.Critical.7",
+  "PF2E_NARRATIVE_FORGE.Narration.Critical.8",
+  "PF2E_NARRATIVE_FORGE.Narration.Critical.9",
+  "PF2E_NARRATIVE_FORGE.Narration.Critical.10",
+  "PF2E_NARRATIVE_FORGE.Narration.Critical.11",
+  "PF2E_NARRATIVE_FORGE.Narration.Critical.12",
+  "PF2E_NARRATIVE_FORGE.Narration.Critical.13",
+  "PF2E_NARRATIVE_FORGE.Narration.Critical.14",
+  "PF2E_NARRATIVE_FORGE.Narration.Critical.15"
 ];
 
 const TARGET_NARRATIONS = {
